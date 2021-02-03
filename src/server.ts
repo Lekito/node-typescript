60581ec4e6ea2f9ba43c4160bc8a73c556aed8a3
Alex Ribeiro Alves <alexlekito075@gmail.com>
2021-02-03 16:02:17 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import routes from './routes';
+import path from 'path'; 
+
 
 const app = express();
 
 app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads'))); // rota estatica
 
 app.listen(3333, () => {
     console.log('Server started on port 3333! Ctrl + c for exit.');
