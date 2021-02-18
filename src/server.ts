9cb6eacf62df44bcd547ab47c29ca3e9aa039eb6
Alex Ribeiro Alves <alexlekito075@gmail.com>
2021-02-18 18:00:03 -0300

    Update server.ts
    Usando o celebrate indicar os erros

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import routes from './routes';
 import path from 'path'; 
+import { errors } from 'celebrate';
 
 const app = express();
 
@@ -16,6 +17,8 @@ app.use(express.json());
 app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads'))); // rota estatica
+
+app.use(errors()); // necessario para gerar os erros que o celebrate de acordo com o que recebeu na requisição. 
 
 app.listen(3333, () => {
     console.log('Server started on port 3333! Ctrl + c for exit.');
